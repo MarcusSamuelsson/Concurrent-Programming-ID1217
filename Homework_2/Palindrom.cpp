3964c7c39dca07fae6ce40418a16d64d2be56094
MarcusSamuelsson <Samuelsson.marcus17@gmail.com>
2023-02-10 19:12:16 +0100

    updated

--- Homework_2/Palindrom.cpp
+++ Homework_2/Palindrom.cpp
@@ -7,12 +7,13 @@
 #include <time.h>
 #include <sys/time.h>
 #include <omp.h>
+#include <cmath>
 
 using namespace std;
 
 
-bool iterateFile(function<void (const string & )> callback) {
-    ifstream wordfile("./Homework_1/words");
+bool iterateFile(function<void (const string & )> callback, string filepath) {
+    ifstream wordfile(filepath);
     if(!wordfile) {
         cout << "No file found";
         return false;
@@ -45,8 +46,8 @@ void toLowerCase (vector<string>& vector) {
     }
 }
 
-void printResultToFile (vector<string>& res) {
-    ofstream resultFile("result");
+void printResultToFile (vector<string>& res, string filename) {
+    ofstream resultFile(filename);
 
     for(int i = 0; i < res.size(); i++) {
         resultFile << res.at(i) << "\n";
@@ -102,30 +103,56 @@ void multiThreadSearch (vector<string> &vec, vector<string> &res) {
         }
     }
 
-    cout << "Total palindromes: " << total;
+    cout << "\nTotal palindromes: " << total;
 }
 
 int main(int argc, char *argv[]) {
     int workers = atoi(argv[1]);
 
-    vector<string> vec;
-    vector<string> result;
+    vector<vector<string>> vec;
+    vector<vector<string>> result;
 
-    bool res = iterateFile([&](const string & str){vec.push_back(str);});
+    vec.resize(3);
+    result.resize(3);
 
-    toLowerCase(vec);
+    iterateFile([&](const string & str){vec.at(0).push_back(str);}, "./Homework_2/words");
+    iterateFile([&](const string & str){vec.at(1).push_back(str);}, "/usr/share/dict/words");
+    iterateFile([&](const string & str){vec.at(2).push_back(str);}, "./Homework_2/tinyWords");
 
-    omp_set_num_threads(workers);
+    toLowerCase(vec.at(0));
+    toLowerCase(vec.at(1));
+    toLowerCase(vec.at(2));
 
-    double start_time = omp_get_wtime();
+    vector<double> timePerThread;
+    timePerThread.resize(workers);
 
-    multiThreadSearch(vec, result);
 
-    double end_time = omp_get_wtime();
 
-    cout << "\nTotal time: " << (end_time-start_time) << " s";
+    for(int x = 1; x <= 3; x++) {
+        cout << "\nTest for file" << x << "\n";
+        for(int i = 1; i <= workers; i++) {
+            double totalForThread = 0;
 
-    printResultToFile(result);
+            for (int j = 0; j < 10; j++) {
+                omp_set_num_threads(i*2);
+
+                double start_time = omp_get_wtime();
+
+                multiThreadSearch(vec.at(x-1), result.at(x-1));
+
+                double end_time = omp_get_wtime();
+
+                totalForThread += (end_time-start_time);
+                cout << "\nTotal time attempt " << j << ": " << (end_time-start_time) << " s";
+            }
+
+            cout << "\nAverage time for " << i*2 << "number of threads: " << (totalForThread/10) << " s";
+        }
+    }
+
+    printResultToFile(result.at(0), "result1");
+    printResultToFile(result.at(1), "result2");
+    printResultToFile(result.at(3), "result3");
 
     return 0;
 }
